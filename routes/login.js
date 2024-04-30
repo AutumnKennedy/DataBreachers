6cf21f6bbdbd0e101c872d338c15b3055ee8aaca
Alyssa Major <alyssamajor2020@gmail.com>
2024-04-30 09:07:38 -0500

    singlethon?

--- routes/login.js
+++ routes/login.js
@@ -4,19 +4,15 @@ const path = require('path');
 const { loginUser} = require('../controllers/authController');
 //const extractTokenFromCookie = require('../middlewares/extractToken');
 const router = express.Router();
-
 router.get('/showLogInForm', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/loginForm.html'));
 });
-
 router.get('/logInRender', (req, res) => {
     loginUser(req, res);
 });
-
 // router.get('/dashboard.html', extractTokenFromCookie, (req, res) => {
 //     res.sendFile('dashboard.html', { root: './views' });
 // });
-
 router.get('/dashboard', (req, res) => {
     res.sendFile("dashboard.html", { root: "./views" })
 });
