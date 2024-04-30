11a71a904622dcbac5fa5fcb8cdb0fd5c14800cc
Alyssa Major <alyssamajor2020@gmail.com>
2024-04-29 22:26:43 -0500

    chatroom form wip cant send

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -5,6 +5,7 @@ const { createTopic } = require('../controllers/topicController');
 router.post('/createTopic', createTopic);
 
 router.get('/chatroom', (req, res) => {
+    // res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Type', 'text/html');
     res.sendFile("chatRoom.ejs", { root: "./views" });
 });
